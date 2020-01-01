0602f5e546eec93e5c9fafbc04ab2172202055d2
kit <smurali05@gmail.com>
2020-01-01 13:18:04 +0530

    initial commit

---
+++ GitTestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world test123");
+            Console.ReadLine();
+        }
+    }
+}
